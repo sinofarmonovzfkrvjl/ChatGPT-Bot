a9cdc52aca475c3e7d27c834f33a5b14e0eab84f
sinofarmonovzfkrvjl <farmonovsino4@gmail.com>
2024-08-26 21:00:31 +0500

    commit

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters import CommandStart
 import logging
-import google.generativeai as genai
 import asyncio
-from keyboards import bot_language, get_audio
+import requests
+from keyboards import bot_language
 
 dp = Dispatcher()
 
@@ -15,22 +15,20 @@ async def start(message: types.Message):
 async def Callback(call: types.CallbackQuery):
     await call.message.delete()
     if call.data == "uz":
-        await call.message.answer(f"Salom <b>{call.message.chat.full_name}</b>\nmen AI English Assistant botman\nme sizge ingliz tilini o'rganishingizga yordam beraman", parse_mode="HTML")
+        await call.message.answer(f"Salom <b>{call.message.chat.full_name}</b>\nmen AI Assistant botman\nme sizge ingliz tilini o'rganishingizga yordam beraman", parse_mode="HTML")
     elif call.data == "en":
-        await call.message.answer(f"Hello <b>{call.message.chat.full_name}</b>\ni'm AI English Assistant Bot\n i can help you to learn english**", parse_mode="HTML")
+        await call.message.answer(f"Hello <b>{call.message.chat.full_name}</b>\ni'm AI Assistant Bot\n i can help you to learn english**", parse_mode="HTML")
     elif call.data == "ru":
-        await call.message.answer(f"Привет <b>{call.message.chat.full_name}</b>\nя бот Английский помощник\nя могу помочь вам узнать английский**", parse_mode="HTML")
+        await call.message.answer(f"Привет <b>{call.message.chat.full_name}</b>\nя бот помощник\nя могу помочь вам узнать английский**", parse_mode="HTML")
 
 @dp.message()
 async def echo(message: types.Message):
-    genai.configure(api_key="")
-    model = genai.GenerativeModel('gemini-1.5-flash')
-    response = model.generate_content(message.text)
-    await message.answer(response.text, parse_mode="MARKDOWN")
+    res = requests.get("https://gemini-api-qnmb.onrender.com/api/v1/generate?word=" + message.text)
+    await message.answer(res.json())
     
 
 async def main():
-    bot = Bot(token="")
+    bot = Bot(token="5904607271:AAFycN_K-pXKmue3YR8zyGYS2Q5A9MZnJzk")
     await dp.start_polling(bot)
 
 if __name__ == '__main__':
